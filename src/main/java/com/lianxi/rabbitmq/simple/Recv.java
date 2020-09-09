9a08ca44065cc142e746f7af83b3397609b454ad
zhao <1562517182@qq.com>
2020-09-09 20:22:49 +0800

    基本消息模型

--- src/main/java/com/lianxi/rabbitmq/simple/Recv.java
+++ src/main/java/com/lianxi/rabbitmq/simple/Recv.java
@@ -1,10 +1,8 @@
 package com.lianxi.rabbitmq.simple;
 
 import com.lianxi.rabbitmq.uitl.ConnectionUtil;
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.*;
+import org.hamcrest.Condition;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -17,6 +15,7 @@ public class Recv {
     private final static String QUEUE_NAME = "simple_queue";
 
     public static void main(String[] args) throws IOException, TimeoutException {
+
         //获取到连接
         Connection connection = ConnectionUtil.getConnection();
 
@@ -27,9 +26,18 @@ public class Recv {
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
         //定义队列中的消费者
-        DefaultConsumer consumer = new DefaultConsumer(channel);
+        DefaultConsumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                //super.handleDelivery(consumerTag, envelope, properties, body);
 
+                String s = new String(body);
 
+                System.out.println("[x]received:"+s+"!");
+            }
 
+        };
+
+        channel.basicConsume(QUEUE_NAME, false, consumer);
     }
 }
